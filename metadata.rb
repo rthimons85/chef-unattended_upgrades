4eb35482ea8016cdccb4186acd3f487dede42d84
rthimons85 <rthimons@gmail.com>
2019-11-13 10:55:07 -0500

    Setting up for a jenkins upload

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name		 "chef-unattended_upgrades"
-maintainer       "Jeremiah Snapp"
-maintainer_email "jeremiah.snapp@gmail.com"
+maintainer       "Robert Thimons"
+maintainer_email "robert.thimons@asburyseminary.edu"
 license          "Apache 2.0"
 description      "Installs/Configures unattended_upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
